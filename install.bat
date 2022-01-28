f2793547d1915a2a2d1452a8fbb1d6ac6296293f
Majap <79178863+Minemagazine@users.noreply.github.com>
2022-01-29 01:53:28 +0900

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,31 @@
+@echo off
+title Discord.py BOT Module Installer
+echo.
+echo.
+echo 스페이스바를 누르면 모듈 설치가 시작됩니다
+echo.
+echo.
 
+pause
+echo.
+echo.
+echo %DATE%
+python --version
+
+python -m pip install -U pip
+pip install discord.py
+pip install discord.py[voice]
+pip install python-dotenv
+pip install logging
+pip install youtube_dl
+pip install datetime
+pip install pytz
+pip install youtube-search-python
+
+echo.
+echo.
+echo 모듈 설치가 끝났습니다.
+echo 아무키나 눌러서 종료하기
+echo.
+echo.
+pause
