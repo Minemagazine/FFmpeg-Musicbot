ec2b3c1ca37d8537b3afb70d9fa16ebbe901a896
Majap <79178863+Minemagazine@users.noreply.github.com>
2022-01-29 01:44:54 +0900

    Update index.py

--- index.py
+++ index.py
@@ -5,14 +5,13 @@ from discord.ext import commands
 
 config = dotenv_values(".env")
 TOKEN = config['TOKEN']
-PERFIX = config['PERFIX']
+PREFIX = config['PREFIX']
 
 intentsa = discord.Intents().all()
-client = commands.Bot(command_prefix= f"{PERFIX}", Intents=intentsa)
+client = commands.Bot(command_prefix= f"{PREFIX}", Intents=intentsa)
 
 
 print('\033[93m-------------------------------------')
-
 for filename in os.listdir("Cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"Cogs.{filename[:-3]}")
@@ -23,20 +22,19 @@ async def on_ready():
     print("\033[95mSystem login SUCCESS")
     print('\u001b[37m')
     server_count = len(client.guilds)
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{PERFIX}help | {server_count} Guilds'))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{PREFIX}help | {server_count} Guilds'))
 
 @client.event
 async def on_command_error(ctx, error):
     logger.warning(f'\033[91m{error}\u001b[37m')
 
-
+# 로깅 설정 ( 로깅 기능이 필요없으면 32~38줄을 지워도 됩니다! )
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('[%(asctime)s %(levelname)s] [%(name)s]: %(message)s', datefmt='%H:%M:%S')
-
-# log 출력
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
+# Client 로그인
 client.run(TOKEN)
