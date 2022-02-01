25a1d71b8b7aefb357f233a8ad69148bf13ec235
Majap <79178863+Minemagazine@users.noreply.github.com>
2022-02-01 23:18:17 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install discord.py
+pip install discord.py[voice]
+pip install python-dotenv
+pip install logging
+pip install youtube_dl
+pip install datetime
+pip install pytz
+pip install youtube-search-python
