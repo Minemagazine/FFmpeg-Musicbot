3214116ac0499648f2cfe6a959a1d64e374c5e1d
Majap <79178863+Minemagazine@users.noreply.github.com>
2022-02-04 10:31:14 +0900

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-pip install discord.py
-pip install discord.py[voice]
-pip install python-dotenv
-pip install logging
-pip install youtube_dl
-pip install datetime
-pip install pytz
-pip install youtube-search-python
